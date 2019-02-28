8ed6130c82d783f8355dd7c330f3601a4c158b6c
Michael Grandorf <mgb@tdc.dk>
2019-02-28 21:27:33 +0100

    Added editorconfig to project

--- fractal-theme/js/src/overview.js
+++ fractal-theme/js/src/overview.js
@@ -1,6 +1,6 @@
 function lastBuild() {
-const lastFractalBuild = document.querySelector('.frctl-last-build-time');
-lastFractalBuild.innerHTML = document.lastModified;
+  const lastFractalBuild = document.querySelector('.frctl-last-build-time');
+  lastFractalBuild.innerHTML = document.lastModified;
 }
 
 export default lastBuild;
