89d56cf1d0e3d9a80a7c614cd8a5479890c8839c
Kim Johannesen <kim@shiftcph.dk>
2019-03-11 15:01:58 +0100

    Adds bow in white and midnight green

---
+++ src/elements/02-elements/bow/bow.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	status: "wip",
+  preview: "@element-preview",
+	label: "Bow",
+	modifierClass: null,
+  darkBg: true,
+  inlineStyle: null,
+	variants: [
+		{
+      name: "default",
+      label: "White"
+    },
+    {
+      name: "Midnight Green",
+      context: {
+        "modifierClass": "ys-bow--midnight-green"
+      }
+    }
+	],
+	"context": {
+    "inlineCss": "body{padding:0;}"
+	}
+};
