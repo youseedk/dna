5d86b7507e25bd022a95f641898385ec79558f1b
Michael Grandorf <mgb@tdc.dk>
2019-03-27 10:09:29 +0100

    Improved accessibility guidelines

--- fractal-theme/assets/js/src/navigation.js
+++ fractal-theme/assets/js/src/navigation.js
@@ -2,8 +2,7 @@ function linkPrettifier() {
   const link = [...document.querySelectorAll('.Tree-entityLink span')];
 
   link.forEach(function(el) {
-   el.textContent = el.textContent.replace('And', '&');
-   console.log(el.textContent);
+   el.textContent = el.textContent.replace('And ', '& ');
   });
 }
 
