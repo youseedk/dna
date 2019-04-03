3b923890b429f015a63783651551364e73fe01e0
Kim Johannesen <kim@shiftcph.dk>
2019-04-03 14:15:25 +0200

    New structure, buttons documentation, copy html

---
+++ fractal-theme/assets/js/src/code-preview.js
@@ -0,0 +1,43 @@
+function addCopyButton(parent) {
+
+  const buttonText = 'Copy to Clipboard';
+  console.log(`${parent.className}--${buttonText}`)
+
+  const button = document.createElement('button');
+  button.textContent = buttonText;
+  button.classList.add('element-preview__button');
+
+  const copyField = document.createElement('input');
+  const textValue = parent.innerHTML;
+  copyField.value = textValue;
+  copyField.classList.add('element-preview__input');
+  parent.appendChild(copyField);
+
+  button.addEventListener('click', (event) => {
+    const elToCopy = copyField;
+    elToCopy.select();
+    document.execCommand("copy");
+  })
+
+  parent.appendChild(button);
+}
+
+function codePreview() {
+  const preEls = document.querySelectorAll('.Prose code pre');
+
+  // wrap preview code in containers
+  preEls.forEach((parentEl) => {
+    parentEl.outerHTML = `<div class="code-preview">${parentEl.outerHTML}</div>`;
+  });
+
+  // add Copy button to all containers — notice: using the live preview here
+  const codePreviews = document.querySelectorAll('.element-preview');
+
+  codePreviews.forEach(element => {
+    addCopyButton(element);
+  })
+
+  //addButton(parentEl.parentNode, btnText);
+}
+
+export default codePreview;
