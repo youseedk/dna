a74ed01b1934efe614ceb8db990d043118c1a25a
Lucas Patrick Dechow <lud@tdc.dk>
2019-11-29 14:03:15 +0100

    Fixed prettier (with a prettier file)

--- fractal-theme/assets/js/src/search-results.js
+++ fractal-theme/assets/js/src/search-results.js
@@ -1,24 +1,24 @@
-import Fuse from "fuse.js";
+import Fuse from 'fuse.js';
 
 function fractalSearchResults() {
-  const searchComponent = document.querySelector(".search");
-  const searchInput = document.getElementById("searchPageInput");
-  const searchResults = document.getElementById("searchResults");
-  const SearchApi = "/assets/theme/js/search.json";
+  const searchComponent = document.querySelector('.search');
+  const searchInput = document.getElementById('searchPageInput');
+  const searchResults = document.getElementById('searchResults');
+  const SearchApi = '/assets/theme/js/search.json';
 
   // Search options
   const searchOptions = {
     keys: [
       {
-        name: "data.title",
+        name: 'data.title',
         weight: 0.5
       },
       {
-        name: "data.primaryKeywords",
+        name: 'data.primaryKeywords',
         weight: 0.4
       },
       {
-        name: "data.secondaryKeywords",
+        name: 'data.secondaryKeywords',
         weight: 0.1
       }
     ]
@@ -34,10 +34,10 @@ function fractalSearchResults() {
       const fuse = new Fuse(search, searchOptions);
       const getSearchParams = new URLSearchParams(location.search);
       function allow_chars_and_nums_only(str) {
-        str = str.replace(/[^\w\s]/gi, "");
+        str = str.replace(/[^\w\s]/gi, '');
         return str.slice(0, 65);
       }
-      const searchString = allow_chars_and_nums_only(getSearchParams.get("q"));
+      const searchString = allow_chars_and_nums_only(getSearchParams.get('q'));
       searchInput.value = searchString;
       searchInput.focus();
 
@@ -54,8 +54,8 @@ function fractalSearchResults() {
       });
 
       // On input
-      searchInput.addEventListener("input", e => {
-        searchResults.innerHTML = "";
+      searchInput.addEventListener('input', e => {
+        searchResults.innerHTML = '';
         const searchValueFuse = fuse.search(e.srcElement.value);
         if (e.srcElement.value) {
           searchValueFuse.forEach(element => {
@@ -76,7 +76,7 @@ function fractalSearchResults() {
     })
     // If an error occurs we will not show the search feature at all
     .catch(err => {
-      console.log("error occured with the search feature in results page");
+      console.log('error occured with the search feature in results page');
       searchComponent.remove();
     });
 }
